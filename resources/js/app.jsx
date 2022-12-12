6d5658781adc5e91093d90695a46c1252bf66a2f
unknown <milosmetlic89@gmail.com>
2022-12-12 21:06:52 +0100

    Added components folder and App,jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,2 +1,9 @@
 import './bootstrap';
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+ReactDOM.createRoot(document.getElementById('app')).render (
+    <App/>
+)
