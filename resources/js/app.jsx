b63df86bebd026b7fcc7eaa493cabc0233521e11
unknown <milosmetlic89@gmail.com>
2022-12-12 20:58:42 +0100

    Remove React components

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,13 +1,2 @@
 import './bootstrap';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-
-ReactDOM.createRoot(document.getElementById('app')).render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>  
-)
-
