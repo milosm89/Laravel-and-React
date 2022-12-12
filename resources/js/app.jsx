51944f23a4c764fa0e5577bf04754e0e4ae2b312
unknown <milosmetlic89@gmail.com>
2022-12-12 21:22:51 +0100

    Add BrowserRouter

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,8 +2,11 @@ import './bootstrap';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('app')).render (
-    <App/>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 )
