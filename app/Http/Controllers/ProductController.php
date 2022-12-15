8ef7edbca7e2d2879fcd82d03b2323c46e7e4e3d
unknown <milosmetlic89@gmail.com>
2022-12-15 17:23:40 +0100

    Added logic to form in New.jsx, ProductController, Model and route

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    //
+    public function add_product(Request $request) {
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+            if ($request->file('photo')) {
+                $uploadedFile = $request->file('photo');
+                
+                if ($uploadedFile) {
+                    $filename = Str::uuid() . '.' . $uploadedFile->getClientOriginalExtension();
+                    Storage::disk('public')->putFileAs('uploadedFiles/', $uploadedFile, $filename);
+                    $product->photo = $filename;
+                }
+            }else {
+                $product->photo = "image.png";
+            }
+        $product->type = $request->type;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->save();
+    }
 }
--- resources/js/components/products/New.jsx
+++ resources/js/components/products/New.jsx
@@ -1,69 +1,116 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const New = () => {
-  return (
-    <div className="container">
-        <div className="products_create">
-            <div className="titlebar">
-                <div className="titlebar_item">
-                    <h1>Add Product</h1>
-                </div>
-                <div className="titlebar_item">
-                    <button className="btn">
-                        Save
-                    </button>
-                </div>
-            </div>
-            <div className="card_wrapper">
-                <div className="wrapper_left">
-                    <div className="card">
-                        <p>Name</p>
-                        <input type="text" />
-                        <p>Description (Optional)</p>
-                        <textarea cols="10" rows="5"></textarea>
-                        <div className="media">
-                            <ul className="images_list">
-                                <li className="image_item">
-                                    <div className="image_item-img">
-                                        <img src="" style={{width: 117, height: 100}} />
-                                    </div>
-                                </li>
-                                <li className="image_item">
-                                    <form action="" className="image_item-form">
-                                        <label className="image_item-form--label">Add Image</label>
-                                        <input type="file" className="image_item-form--input" />
 
-                                    </form>
-                                </li>
-                            </ul>
+    const navigate = useNavigate()
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [photo, setPhoto] = useState(null);
+    const [preview, setPreview] = useState(null);
+    const [type, setType] = useState("")
+    const [quantity, setQuantity] = useState("")
+    const [price, setPrice] = useState("")
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        // Create a FormData object
+        const formData = new FormData();
+
+        // Append file to the formData object here
+        formData.append('name', name)
+        formData.append('description', description)
+        formData.append('photo', photo)
+        formData.append('type', type)
+        formData.append('quantity', quantity)
+        formData.append('price', price)
+
+        try {
+            // We will send formData object as a data to the API URL here.
+            const response = await axios.post("/api/add_product", formData, {
+                headers: {"Content-Type": "multipart/form-data"}
+            }).then((res) => {
+                alert("File Uploaded Successfully");
+            }).catch((error) => {
+                alert("Error")
+            });
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleFileSelect = (event) => {
+        var file = event.target.files[0];
+        var reader = new FileReader();
+        var url = reader.readAsDataURL(file);
+        reader.onloadend = function (e) {
+            setPreview(reader.result)
+        }
+        setPhoto(event.target.files[0])
+    }
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <div className="products_create">
+                    <div className="titlebar">
+                        <div className="titlebar_item">
+                            <h1>Add Product</h1>
+                        </div>
+                        <div className="titlebar_item">
+                            <input className='btn' type="submit" value="Save"/>
                         </div>
                     </div>
-                </div>
-                <div className="wrapper_right">
-                    <div className="card">
-                        <p>Product Type</p>
-                        <input type="text" />
-                        <hr className="hr" />
-                        <p>Inventory</p>
-                        <input type="text" />
-                        <hr className="hr" />
-                        <p>Price</p>
-                        <input type="text" />
-                        <div className="br"></div>
+                    <div className="card_wrapper">
+                        <div className="wrapper_left">
+                            <div className="card">
+                                <p>Name</p>
+                                <input type="text" value={name} onChange={(event)=>{setName(event.target.value)}} />
+                                <p>Description (Optional)</p>
+                                <textarea cols="10" rows="5" value={description} onChange={(event)=>{setDescription(event.target.value)}}></textarea>
+                                <div className="media">
+                                    <ul className="images_list">
+                                        <li className="image_item">
+                                            <div className="image_item-img">
+                                                <img src={preview} style={{width: 117, height: 100}} />
+                                            </div>
+                                        </li>
+                                        <li className="image_item">
+                                            <label className="image_item-form--label">Add Image</label>
+                                            <input type="file" className="image_item-form--input" onChange={handleFileSelect} />
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="wrapper_right">
+                            <div className="card">
+                                <p>Product Type</p>
+                                <input type="text" value={type} onChange={(event)=>{setType(event.target.value)}} />
+                                <hr className="hr" />
+                                <p>Inventory</p>
+                                <input type="text" value={quantity} onChange={(event)=>{setQuantity(event.target.value)}} />
+                                <hr className="hr" />
+                                <p>Price</p>
+                                <input type="text" value={price} onChange={(event)=>{setPrice(event.target.value)}}/>
+                                <div className="br"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="titlebar">
+                        <div className="titlebar_item"></div>
+                            <div className="titlebar_item">
+                                <input className='btn' type="submit" value="Save"/>
+                            </div>
                     </div>
                 </div>
-            </div>
-            <div className="titlebar">
-                <div className="titlebar_item"></div>
-                    <div className="titlebar_item">
-                        <button className="btn">
-                            Save
-                        </button>
-                    </div>
-            </div>
-        </div>
+            </form>
     </div>
-  )
-}
+        
+    )
+};
 
-export default New
+export default New;
